5a9fb70551dd8556e14a341e5aba637fb41fb8de
Apoorva Chauhan <47474447+apoorvacha@users.noreply.github.com>
2023-02-05 05:58:15 +0530

    Update TestCases.py

--- TestCases.py
+++ TestCases.py
@@ -1,15 +1,29 @@
-from Num import Num
+import math 
+class Num:
+    def __init__(self, n =0, mu =0, m2=0):
+        self.n = n
+        self.mu = mu
+        self.m2 = m2
+        self.lo = float('inf')
+        self.hi = float('-inf')
 
 
-def test_nums():
-    val = Num()
-    lst = [1, 2, 3, 4, 5, 6, 7]
-    for a in lst:
-        val.add(a)
-    # print(val.m2, val.n, val.mu)
-    # print("in test class")
-    # print(val.mid(0), round(val.div(0)))
-    return (1 == val.mid(0)) and (4.0 == round(val.div(0)))
+    def add(self, n):
+        if n !="?":
+            self.n = self.n + 1
+            d = n - self.mu
+            self.mu = self.mu + (d / self.n)
+            self.m2 = self.m2 + (d * (n - self.mu))
+            self.lo = min(n, self.lo)
+            self.hi = max(n, self.hi)
 
 
-test_nums()
+    def mid(self):
+        return self.mu # n; return mean
+
+    #n; return standard deviation using Welford's algorithm 
+    def div(self): 
+        if self.m2 < 0 or self.n < 2:
+            return 0
+        else:
+            return pow((self.m2/(self.n-1)),0.5)
